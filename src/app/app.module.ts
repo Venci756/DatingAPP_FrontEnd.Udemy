b7d92ebd5678c3d8099d9d78a0859a08ba6449cc
Venko Trajkovski <infidel756@gmail.com>
2020-07-18 15:11:22 +0200

    End of section 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,20 +11,29 @@ import { HomeComponent } from './home/home.component';
 import {ErrorInterceptorProvider} from "./_services/error.interceptor";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import { MembersListComponent } from './members-list/members-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./routes";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    MembersListComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ErrorInterceptorProvider,
